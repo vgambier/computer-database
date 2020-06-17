91d343ec682c9ea9f7f6d2060244a5387712f583
Victor Gambier <vgambier@excilys.com>
2020-06-17 16:21:11 +0200

    Added empty CompanyPage and Page classes

---
+++ src/main/java/model/CompanyPage.java
@@ -0,0 +1,5 @@
+package model;
+
+public class CompanyPage implements Page {
+	//TODO: write this class
+}
---
+++ src/main/java/model/Page.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface Page {
+	// TODO: write this interface
+}
