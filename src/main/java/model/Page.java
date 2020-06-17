c47c965a9d66f6aa5f0788ca3dac8217e46d3a70
Victor Gambier <vgambier@excilys.com>
2020-06-17 10:50:30 +0200

    Redid project architecture

---
+++ src/main/java/model/Page.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Page {
+
+	private static final int MAX_ITEMS_PER_PAGE = 25;
+
+	public Page() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/ui/Test.java
@@ -0,0 +1,9 @@
+package ui;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
