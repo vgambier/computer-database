5c1d32d8f1cb4e5e1a92ba818681943a61165539
FlorentBergere <fbergere@excilys.com>
2020-08-07 17:18:10 +0200

    ajout @CrossOrigin

--- webservice/src/main/java/com/excilys/cdb/controller/DashboardRESTController.java
+++ webservice/src/main/java/com/excilys/cdb/controller/DashboardRESTController.java
@@ -3,6 +3,7 @@ package com.excilys.cdb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import exception.PageNotFoundException;
  * @author Victor Gambier
  *
  */
+@CrossOrigin
 @RestController
 public class DashboardRESTController {
 
