522c66c887821296820ca118d4957b0877b9d76c
FlorentBergere <fbergere@excilys.com>
2020-08-20 16:03:44 +0200

    username unique

--- core/src/main/java/com/excilys/cdb/model/User.java
+++ core/src/main/java/com/excilys/cdb/model/User.java
@@ -17,7 +17,7 @@ public class User implements Serializable {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
 
     @Column(name = "password")
